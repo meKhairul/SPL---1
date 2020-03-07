5a0078a61e0c926c08f4f7a039dabdf143394df7
Khairul Alam <48632522+meKhairul@users.noreply.github.com>
2020-03-07 14:03:35 +0600

    update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -5,11 +5,12 @@ void menu()
    int t;
    while(1)
    {
-       cout << "\t\t\t\t\t1.Start Test\n\t\t\t\t\t2.Performance";
+       cout << "\t\t\t\t\t1.Start Test\n\t\t\t\t\t2.Performance\n";
        cin>>t;
        if(t==1)
        {
-           Test();
+           readFile();
+
        }
        if(t==2)
        {
