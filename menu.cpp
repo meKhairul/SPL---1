a5cad4c52af6c24ee70b5076b639bde3d1d639c0
Khairul Alam <48632522+meKhairul@users.noreply.github.com>
2021-02-11 23:17:13 +0600

    Refactored

--- menu.cpp
+++ menu.cpp
@@ -5,20 +5,27 @@ void menu()
    int t;
    while(1)
    {
-       cout << "\t\t\t\t\t1.Start By 4 Level Test\n\t\t\t\t\t2.Test By Rasch Algorithm\n\t\t\t\t\t3.Exit\n";
+       cout << "\t\t\t\t\t1.Start By 4 Level Test\n\t\t\t\t\t2.Test By Rasch Algorithm\n\t\t\t\t\t3.Test By Monte Carlo Algo\n\t\t\t\t\t0.Exit\n";
        cin>>t;
        if(t==1)
        {
+           cout << "If You Want To Skip Question Write \'SKIP\' \n";
            readFile();
 
        }
-       if(t==2)
+       else if(t==2)
        {
+           cout << "If You Want To Skip Question Write \'SKIP\' \n";
            rasch();
        }
-       if(t==3)
+       else if(t==0)
        {
            exit(0);
+       }
+       else if(t==3)
+       {
+           cout << "If You Want To Skip Question Write \'SKIP\' \n";
+           monte();
        }
    }
 
